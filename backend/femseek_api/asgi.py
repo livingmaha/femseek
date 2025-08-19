2fb2b3056df80a9cd535cbc16a7d25b50cc336ae
livingmaha <sharrah100b@gmail.com>
2025-08-19 14:13:51 +0300

    Update asgi.py

--- backend/femseek_api/asgi.py
+++ backend/femseek_api/asgi.py
@@ -1,1 +1,16 @@
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import translator.routing
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'femseek_api.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            translator.routing.websocket_urlpatterns
+        )
+    ),
+})
